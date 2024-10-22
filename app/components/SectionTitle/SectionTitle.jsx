d5b8a6b6721052ad969247e4ffef01583eac6bdd
Rashadhusien <rashadhusein440@gmail.com>
2024-10-22 13:38:57 +0300

    add skills

--- app/components/SectionTitle/SectionTitle.jsx
+++ app/components/SectionTitle/SectionTitle.jsx
@@ -1,3 +1,4 @@
+"use client";
 import "./style.css";
 import { motion } from "framer-motion";
 
--- app/page.js
+++ app/page.js
@@ -4,9 +4,8 @@ import Hero from "./pages/about/page";
 import { useEffect, useState } from "react";
 import Main from "./pages/projects/page";
 import Contact from "./pages/contactus/page";
+import Skills from "./pages/skills/page";
 import { motion, useScroll, useSpring } from "framer-motion";
-
-// import Skills from "./pages/skills/page";
 
 export default function Home() {
   useEffect(() => {
@@ -37,16 +36,15 @@ export default function Home() {
 
       <Hero />
       <div className="border-b border-borderlight dark:border-border my-12 w-[200%] -ml-16" />
-      {/*
-        
-       <Skills /> 
+
+      <Skills />
+
       <div className="border-b border-borderlight dark:border-border my-12 w-[200%] -ml-16" />
-*/}
+
       <Main />
       <div className="border-b border-borderlight dark:border-border my-12 w-[200%] -ml-16" />
       <Contact />
       <div className="border-b border-borderlight dark:border-border my-12 w-[200%] -ml-16" />
-      {/* <Footer /> */}
       <a
         href="#up"
         className={`bg-scrolltop  border-scrolltopborder hover:bg-scrollhover dark:bg-amber-400 dark:hover:bg-orange duration-300 fixed bottom-10 right-[2%] rounded-full sm:w-10 sm:h-10 w-8 h-8 text-2xl sm:opacity-100 opacity-100  hover:opacity-100`}
